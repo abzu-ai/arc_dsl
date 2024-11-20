c7c1e817eb387b9c6a5d3abba8bcae7930b8a5e2
Tom Jelen <tom@jelen.dk>
2024-11-20 12:10:46 +0100

    add tests for canonicalize
    most importantly for symbol-preservation

--- test/test_gridlisp.py
+++ test/test_gridlisp.py
@@ -1,7 +1,8 @@
 import pytest
 import numpy as np
 
-from arc_dsl.interpreter import parse_sentence, eval_program
+from arc_dsl.errors import ParseError
+from arc_dsl.interpreter import parse_sentence, eval_program, canonicalize
 
 from .gridlisp_examples import gridlisp_examples
 
@@ -23,3 +24,29 @@ def test_dsl_eval(sentence, expected_output):
         assert isinstance(
             e, expected_output
         ), f"Expected exception {expected_output.__name__}, but got {e.__class__.__name__}"
+
+
+def test_canonicalize():
+    # Capitalizes symbols
+    assert canonicalize("var1") == "VAR1"
+
+    # Strips comments
+    assert canonicalize("(SETPARAM VAR1 3) ;; I am a comment!") == "(SETPARAM VAR1 3)"
+
+    # Strips whitespace
+    assert canonicalize(" ( SETPARAM VAR1 3 ) ") == "(SETPARAM VAR1 3)"
+
+    # Separates forms with newlines for readability
+    assert (
+        canonicalize("(SETPARAM VAR1 3) (SETPARAM VAR2 3)")
+        == "(SETPARAM VAR1 3)\n(SETPARAM VAR2 3)"
+    )
+
+    # Allows preserving non-dsl symbols
+    assert (
+        canonicalize("(SETPARAM VAR1 ?)", preserve_these=["?"]) == "(SETPARAM VAR1 ?)"
+    )
+
+    # Raises on unknown symbols
+    with pytest.raises(ParseError):
+        canonicalize("(SETPARAM VAR1 ?)", preserve_these=["!"])
